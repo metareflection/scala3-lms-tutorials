4a0465b2b375ca23e7139430093a3405b6b98357
Cameron Wong <cwong@g.harvard.edu>
2025-05-29 16:00:14 -0400

    Squashed 'lms3/' changes from 89c1598..cc12646
    cc12646 fix handling of nested arrays
    
    git-subtree-dir: lms3
    git-subtree-split: cc12646c304d6160d7b8659256df917213b5c2e9

---
+++ src/out/manifest/nested-array-formatting.check.scala
@@ -0,0 +1,11 @@
+/*****************************************
+Emitting Generated Code
+*******************************************/
+class Snippet extends ((Array[Array[Int]])=>(Int)) {
+  def apply(x0:Array[Array[Int]]): Int = {
+    1
+  }
+}
+/*****************************************
+End of Generated Code
+*******************************************/
